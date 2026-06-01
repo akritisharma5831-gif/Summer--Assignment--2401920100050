177ae48e2803ddb5880a137815968a697d2e3ee9
akritisharma5831-gif <akritisharma5831@gmail.com>
2026-06-01 23:27:17 +0530

    Update Best-time-to-buy-and-sell-stock.java

--- week1/day1/Best-time-to-buy-and-sell-stock.java
+++ week1/day1/Best-time-to-buy-and-sell-stock.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++)
+{
+   int currprofit=prices[i]-min; 
+   if(currprofit>profit){
+    profit=currprofit;
+   }
+    
+    if(prices[i]<min){
+        min=prices[i];
+    }
+    }
+return profit;
+ 
+    }
+}
