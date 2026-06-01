8200dfea67547cec69c32de7c0f84014c3205f48
akritisharma5831-gif <akritisharma5831@gmail.com>
2026-06-01 22:12:11 +0530

    Update twoSum.java

--- week1/day1/twoSum.java
+++ week1/day1/twoSum.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int c[]=new int[2];
+        int a=0;
+        int b=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        c[0]=a;
+        c[1]=b;
+        return c;
+    }
+}
